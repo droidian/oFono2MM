1caeecd8b8915136c79cb19713aefd8218da9ff2
Erik Inkinen <erik.inkinen@gmail.com>
2021-06-05 14:25:38 +0300

    Fix Python module definition

--- ofono2mm/__init__.py
+++ ofono2mm/__init__.py
@@ -1,13 +1,17 @@
 from .mm_modem_3gpp import *
 from .mm_modem_messaging import *
 from .mm_modem import *
+from .mm_modem_simple import *
 from .mm_sim import *
+from .mm_bearer import *
 from .mm_sms import *
 
 __all__ = [
 	"MMModem3gppInterface",
 	"MMModemInterface",
 	"MMModemMessagingInterface",
+	"MMModemSimpleInterface",
 	"MMSimInterface",
+	"MMBearerInterface",
 	"MMSmsInterface",
 ]
