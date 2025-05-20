e6e8ed85c5f66f3eb8f3a154e258342f14f17fb6
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2025-05-20 18:16:21 +0200

    mm_modem_messaging: Simplify and improve SMS D-Bus path handling
    Refactor the way SMS D-Bus paths are computed and returned. Instead of using a temporary variable to preserve the path value across operations, compute the bus_path earlier and reuse it consistently throughout the method.

--- ofono2mm/mm_modem_messaging.py
+++ ofono2mm/mm_modem_messaging.py
@@ -79,17 +79,17 @@ class MMModemMessagingInterface(ServiceInterface):
             'DeliveryReportRequest': properties['delivery-report-request'] if 'delivery-report-request' in properties else Variant('b', False)
         })
 
-        self.bus.export(f'/org/freedesktop/ModemManager1/SMS/{message_i}', mm_sms_interface)
-        self.props['Messages'].value.append(f'/org/freedesktop/ModemManager1/SMS/{message_i}')
+        bus_path = f'/org/freedesktop/ModemManager1/SMS/{message_i}'
+        self.bus.export(bus_path, mm_sms_interface)
+        self.props['Messages'].value.append(bus_path)
         self.emit_properties_changed({'Messages': self.props['Messages'].value})
-        self.Added(f'/org/freedesktop/ModemManager1/SMS/{message_i}', True)
-        message_i_old = message_i
+        self.Added(bus_path, True)
         message_i += 1
 
         if 'org.ofono.MessageManager' in self.ofono_interfaces:
             ofono_sms_path = await self.ofono_interfaces['org.ofono.MessageManager'].call_send_message(properties['number'].value, properties['text'].value)
 
-        return f'/org/freedesktop/ModemManager1/SMS/{message_i_old}'
+        return bus_path
 
     @signal()
     def Added(self, path, received) -> 'ob':
